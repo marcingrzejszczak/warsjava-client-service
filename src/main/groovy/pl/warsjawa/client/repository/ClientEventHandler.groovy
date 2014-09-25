63fecffd9f279490d1773ad2671bcab12ec4a09a
mchmielarz <michal.chmielarz@gmail.com>
2014-09-25 14:47:53 +0200

    Proper call to reporting service.

--- src/main/groovy/pl/warsjawa/client/repository/ClientEventHandler.groovy
+++ src/main/groovy/pl/warsjawa/client/repository/ClientEventHandler.groovy
@@ -1,15 +1,12 @@
 package pl.warsjawa.client.repository
+
 import com.ofg.infrastructure.web.resttemplate.fluent.ServiceRestClient
-import groovy.json.JsonBuilder
 import org.springframework.data.rest.core.annotation.HandleAfterCreate
-import org.springframework.data.rest.core.annotation.HandleAfterDelete
-import org.springframework.data.rest.core.annotation.HandleAfterSave
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler
 import pl.warsjawa.client.domain.Client
 
 import static pl.warsjawa.client.ClientServiceApi.REPORTING_API_VERSION_1
 import static pl.warsjawa.client.Dependencies.REPORTING
-import static pl.warsjawa.client.repository.ClientEventHandler.ActionType.*
 
 @RepositoryEventHandler(Client)
 class ClientEventHandler {
@@ -23,8 +20,8 @@ class ClientEventHandler {
     @HandleAfterCreate void handleAfterCreate(Client client) {
         serviceRestClient.forService(REPORTING.toString())
                 .post()
-                .onUrlFromTemplate('/clients')
-                .body(buildCreateRequestBody(client, actionType))
+                .onUrl('/clients')
+                .body(buildCreateRequestBody(client))
                 .withHeaders()
                     .contentType(REPORTING_API_VERSION_1)
                 .andExecuteFor()
@@ -42,7 +39,4 @@ class ClientEventHandler {
         return builder.toString()
     }
 
-    enum ActionType {
-        CREATE, SAVE, DELETE
-    }
 }
